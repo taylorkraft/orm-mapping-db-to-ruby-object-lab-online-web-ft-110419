362942ccfc82816ea4b4919f29b8a5eb3e26305c
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-26 20:09:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,9 +88,9 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10
     sql = <<-SQL
-    SELECT (x)
+    SELECT *
     FROM students
     WHERE grade = 10
     SQL
