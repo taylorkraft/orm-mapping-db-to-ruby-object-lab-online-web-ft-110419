f0fcf6d64de0cc1c2dcec0cbdf561525890f13c0
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-26 20:18:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -104,14 +104,13 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(x)
     sql = <<-SQL
     SELECT *
     FROM students
-    WHERE grade = X
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
