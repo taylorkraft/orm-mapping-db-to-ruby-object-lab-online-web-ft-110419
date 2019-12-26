e1a7240ea3648659a2403a0490291d2b35251700
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-26 19:41:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,9 +24,10 @@ class Student
     WHERE name = ?
     LIMIT 1
     SQL
+    
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
 
   def save
